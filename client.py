d76d3964e84d119b868dd2562dc90bfb7f725657
Elnurhan <gerievelnur@gmail.com>
2020-05-04 12:48:30 +0300

    FIX #1

--- client.py
+++ client.py
@@ -1,4 +1,53 @@
 import socket
+import time
 
-with socket.create_connection(('127.0.0.1', 10001)) as sock:
-    sock.sendall('ping'.encode('utf-8'))
+
+class ClientError(Exception):
+    '''Выбрасывается в случае неуспешной отправки'''
+    pass
+
+
+class Client:
+    def __init__(self, ip, port, timeout=None):
+        self.ip = ip
+        self.port = port
+        try:
+            self.connection = socket.create_connection((self.ip, self.port), timeout)
+        except socket.error as err:
+            print(err)
+
+    def _response_parser(self, resp):
+        #message = self.connection.recv(1024).decode()
+        message = resp.decode()
+        response = message.split()
+        if response[0] == 'error':
+            raise ClientError
+        elif len(response) == 4:
+            print({})
+
+
+    def put(self, name, value, timestamp=int(time.time())):
+        request = f'put {name} {value} {timestamp}\n'
+        try:
+            self.connection.sendall(request.encode("utf-8"))
+        except socket.error as err:
+            raise ClientError("Client error: Put")
+
+        self._response_parser(self.connection.recv(1024))
+
+    def get(self, name):
+        request = f'get {name}\n'
+        try:
+            self.connection.sendall(request.encode("utf-8"))
+        except socket.error as err:
+            raise ClientError("Client Error: Get")
+        
+        self._response_parser(self.connection.recv(1024))
+        #return self.connection.recv(1024).decode()
+
+
+    def close(self):
+        try:
+            self.connection.close()
+        except socket.error as err:
+            print(err)
